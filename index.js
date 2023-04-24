8d1eb5e9ae3ceed0ecc9c85e44d86c29cf4bd678
silvak <silvak.jeg@gmail.com>
2023-04-24 12:28:52 -0400

    add cors & vercel config

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // url webs directions
 const URLS = {
