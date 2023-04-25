7f76de1ab98ee9eb151dd7a2250baa266b18dc9d
silvak <silvak.jeg@gmail.com>
2023-04-25 00:14:43 -0400

    change the local url

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const corsProxy = createProxyMiddleware({
 // Ruta original sin el proxy inverso
 app.get("/web", async (req, res) => {
   try {
-    const response = await axios.get("http://localhost:3000");
+    const response = await axios.get("http://news-api-kohl.vercel.app/web");
     const $ = cheerio.load(response.data);
     $(".x-ph").remove(); // Eliminar todos los elementos con la clase 'clase-a-eliminar' del DOM
     $(".ad-giga").remove();
